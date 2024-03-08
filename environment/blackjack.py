f43ab2eea1fe5617b7f90ade81bb182fc63aca20
Nick de Vries <nick.devries@xs4all.nl>
2024-03-09 00:27:42 +0100

    Added vectorization

--- environment/blackjack.py
+++ environment/blackjack.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np 
 
 def create_deck():
     """Create a shuffled deck of 52 cards."""
@@ -41,6 +42,15 @@ def evaluate_game(player_hand, dealer_hand, player_money, current_bet):
         return player_money - current_bet, "Dealer wins!"
     else:
         return player_money, "Push!"
+
+def get_state_representation(player_hand, dealer_visible_card):
+    """Represents the game state as a one-hot encoded vector."""
+    state = np.zeros(52 + 1 + 13)  # 13 for each card value, 1 for num aces, 13 for dealer's card
+    for card in player_hand:
+        state[card - 1] = 1
+    state[26 + len(player_hand.count(11))] = 1  
+    state[39 + dealer_visible_card - 1] = 1 
+    return state
 
 def start_game(player_money, base_bet):
     """Initialize the game, deal initial cards."""
